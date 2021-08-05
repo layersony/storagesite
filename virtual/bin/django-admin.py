e24f61c9aee3cbee9d47c89ff8b5dbebac3dafea
Tajeu2001 <tajeusanta@gmail.com>
2021-08-05 15:15:12 +0300

    Add the virtual environment in to the gitignore file

--- virtual/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/moringa/Documents/Moringa Pro-Dev/storagesite/virtual/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
