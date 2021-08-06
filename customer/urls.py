c155140d672560ef98a80fdf7a9581c6f87a5598
gabriel <ayuelgarang105@gmail.com>
2021-08-06 16:31:26 +0300

    Added profile link to the url

--- customer/urls.py
+++ customer/urls.py
@@ -1,6 +1,12 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
   path('', views.index, name='custhome'),
+  path('', views.index, name='index'),
+  path('account/', include('django.contrib.auth.urls')),
+  path('profile/<username>', views.profile, name='profile'),
+  path('profile/<username>/edit/', views.updateProfile, name='updateProfile'),
 ]
