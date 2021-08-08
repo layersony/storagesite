92448cef84a1764e2e2aae9741aec3208826b6eb
ROSEMARY TAJEU <tajeusanta7@gmail.com>
2021-08-08 10:09:00 +0300

    Enable the employee  to add a user in the onsite booking form

--- employee/forms.py
+++ employee/forms.py
@@ -2,9 +2,16 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.db.models.base import Model
 from .models import User ,Profile,Booking,Unit
-
+from mainapp.views import AddProfileForm
 
 class BookingForm(forms.ModelForm):
     class Meta:
         model = Booking
         fields ='__all__'    
+
+
+class AddUserForm(forms.ModelForm):
+    password1 = forms.CharField(label = "Password", required=True, widget=forms.TextInput(attrs={'placeholder':'Password', 'class': 'form-control', 'type':'password'}))
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'name', 'user_type', 'is_staff', 'is_superuser', 'is_active', 'password1']
--- employee/views.py
+++ employee/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render ,redirect,get_object_or_404
 from django.http import HttpResponseRedirect
-from . forms import BookingForm
+from . forms import BookingForm,AddUserForm
 from .models import User ,Profile,Booking,Unit
-
+from mainapp import views
 
 def units(request):
     return render (request,'employee/units.html')
@@ -12,11 +12,22 @@ def onsite_booking(request):
     users = User.objects.exclude(id=request.user.id)
     if request.method == 'POST':
         form = BookingForm(request.POST, request.FILES)
+        user_form = AddUserForm(request.POST)
+
         if form.is_valid():
             upload = form.save(commit=False)
             upload.user = request.user.profile
             upload.save()
             return HttpResponseRedirect(request.path_info)
+
+        if user_form.is_valid():
+            add_user = user_form.save(commit=False)
+            add_user.set_password(user_form.cleaned_data['password1'])
+            add_user.save()
+            return  redirect('onsite_booking') 
     else:
         form = BookingForm()
-    return render(request, 'employee/onsite_booking.html', {'form': form,'users': users,})
+        views.customadmin
+        user_form = AddUserForm()
+    return render(request, 'employee/onsite_booking.html', { 'user_form': user_form, 'form': form,'users': users,})
+
