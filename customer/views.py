05b22e636770edf869486c6e95151da5b46c67a1
AbdiazizH <a.aziz69890@gmail.com>
2021-08-09 09:22:38 +0300

    create veiws for customer

--- customer/views.py
+++ customer/views.py
@@ -1,13 +1,48 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render ,redirect,get_object_or_404
+from django.http import HttpResponseRedirect
+from .forms import BookingForm,PaymentForm
+
+
+from .models import User ,Profile,Booking,Unit
 
 def available(request):
-      return render(request, 'all_customer/available_units.html', )
+      units=Unit.objects.all()
+      return render(request, 'all_customer/available_units.html', {"units":units})
 
 
-def book(request):
-      return render(request, 'all_customer/book.html', )
+
+def book(request, pk=None):
+      if pk:
+            unit=Unit.objects.get(pk=pk)
+      else:
+            unit = request.user
+      
+      form_class = BookingForm()
+      form = BookingForm()
+
+      if request.method == 'POST':
+            form = BookingForm(request.POST)
+            if form.is_valid():
+                  form.save()
+
+      context = {'form': form}
+      return render(request, 'all_customer/book.html',  context)
 
 
 def payment(request):
-      return render(request, 'all_customer/payment.html', )
+      form_class = PaymentForm()
+      form = PaymentForm()
+
+      if request.method == 'POST':
+            form = PaymentForm(request.POST)
+            if form.is_valid():
+                  form.save()
+
+      context = {'form': form}
+      return render(request, 'all_customer/payment.html', context)
+
+
+
+
+      
+      
